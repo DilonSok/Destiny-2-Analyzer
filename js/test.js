b6d21b7b0ad4b7f70b6b605a3883e1bcab410691
Dilon Sok <sokdilon@gmail.com>
2023-07-08 13:57:29 -0500

    Testing With Api
    Tested various API Calls.
    
    Next step is to set up basic HTML with login and authentication to pull selected character to begin analyzing.
    
    Implement that step with basic API call to test if character was pulled or not.

--- js/test.js
+++ js/test.js
@@ -1,17 +1,18 @@
 var apiKey = "c9501b1afe3a4d879887457002eaf19d";
-
+const tester = document.getElementById('test');
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "https://www.bungie.net/platform/Destiny/Manifest/InventoryItem/1274330687/", true);
+xhr.open("GET", "https://www.bungie.net//Platform/Destiny2/3/Account/4611686018492293152/Character/2305843009522836757/Stats/?groups=2/", true);
 xhr.setRequestHeader("X-API-Key", apiKey);
 
-const tester = document.getElementById('test');
+xhr.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    var json = JSON.parse(this.responseText);
+    console.log(json); 
 
-xhr.onreadystatechange = function(){
- if(this.readyState === 4 && this.status === 200){
-  var json = JSON.parse(this.responseText);
-  console.log(json.Response.data.inventoryItem.itemName); //Gjallarhorn
-  test.innerText = json.Response.data.inventoryItem.itemName;
- }
+    var act_cleared = json.Response.allPvE.allTime.activitiesCleared.basic.value;
+    tester.innerText = "All-Time Activities Cleared (PvE): " + JSON.stringify(act_cleared);
+  }
+ 
 }
 
 xhr.send();
