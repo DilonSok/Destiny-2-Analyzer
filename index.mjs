14e79be6d937dee3bff6a6486c6018ca30ae7dc5
Dilon Sok <sokdilon@gmail.com>
2024-09-01 14:58:11 -0500

    located missing files

--- index.mjs
+++ index.mjs
@@ -1,8 +1,9 @@
 import fetch from 'node-fetch';
 import express from 'express';
+import Datastore from 'nedDb';
 import dotenv from 'dotenv';
-
 dotenv.config();
+
 
 const app = express();
 const port = 3000;
@@ -14,7 +15,11 @@ app.listen(port, () => {
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
-app.post('/d2analyzer/search', async (req, res) => {
+const database = new Datastore('datastore.db');
+database.loadDataBase();
+
+//Request for name search (ex. Waffle#4915)
+app.post('/d2wrapped/searchName', async (req, res) => {
   const url = 'https://www.bungie.net/platform/Destiny2/SearchDestinyPlayerByBungieName/All/';
   const apiKey = process.env.API_KEY;
   const displayName = req.body.displayName;
@@ -47,3 +52,85 @@ app.post('/d2analyzer/search', async (req, res) => {
   }
 
 });
+
+
+app.post;
+
+//getting characterIds
+app.get('/d2wrapped/getCharacters', async (req, res) => {
+
+  const membershipType = req.body.membershipType;
+  const destinyMembershipId = req.body.destinyMembershipId;
+  const components = req.body.component;
+  const url = `https://www.bungie.net/platform/Destiny2/${membershipType}/Profile/${destinyMembershipId}/?component=${components}`;
+  const apiKey = process.env.API_KEY;
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'X-API-KEY': apiKey,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        destinyMembershipId: membershipId,
+        membershipType: membershipType,
+        component: Characters
+      })
+    });
+    if (response.ok) {
+      const data = await response.json();
+      res.json(data);
+    }
+    else {
+      console.error(`Request failed with status ${response.status}`);
+      res.status(response.status).send('Request failed');
+    }
+
+  }
+  catch (error) {
+    console.error('Error: ', error);
+  }
+
+});
+
+app.get;
+
+//get historical data
+app.get('/d2wrapped/getHistoricalData', async (req, res) => {
+
+  const membershipType = req.body.membershipType;
+  const destinyMembershipId = req.body.destinyMembershipId;
+  const components = req.body.component;
+  const url = `https://www.bungie.net/platform/Destiny2/${membershipType}/Profile/${destinyMembershipId}/`;
+  const apiKey = process.env.API_KEY;
+
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'X-API-KEY': apiKey,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        destinyMembershipId: membershipId,
+        membershipType: membershipType,
+      })
+    });
+    if (response.ok) {
+      const data = await response.json();
+      res.json(data);
+      //load data into database?
+    }
+    else {
+      console.error(`Request failed with status ${response.status}`);
+      res.status(response.status).send('Request failed');
+    }
+
+  }
+  catch (error) {
+    console.error('Error: ', error);
+  }
+
+});
+
--- public/js/search.js
+++ public/js/search.js
@@ -6,7 +6,7 @@ document.getElementById("search-button").addEventListener("click", async () => {
     displayNameCode = input[1];
 
     try {
-        const response = await fetch('/d2analyzer/search', {
+        const response = await fetch('/d2wrapped/searchName', {
             method: 'POST',
             headers: {
                 'Content-type': 'application/json'
@@ -22,16 +22,87 @@ document.getElementById("search-button").addEventListener("click", async () => {
             console.log(data.Response); // Access the 'Response' property
             console.log(data.Message); // Access the 'Message' property
             console.log(data.MessageData); // Access the 'MessageData' property
+            const membershipId = data.Response.UserInfoCard.membershipId;
+            const membershipType = data.Response.UserInfoCard.membershipType;
         }
         else {
             console.error(`Request failed with status ${response.status}`);
         }
+
+
+    }
+    catch (error) {
+        console.error('An error occurred:', error);
+    }
+
+
+    //get all characterId's
+    try {
+        const response = await fetch('/d2wrapped/getCharacters', {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                destinyMembershipId: membershipId,
+                membershipType: membershipType,
+                component: Characters
+            })
+        });
+    
+        if (response.ok) {
+            const data = await response.json();
+            const characterIds = [];
+            const characterData = data.Response.Characters;
+
+            //Creating array of CharacterId's
+            if(Array.isArray(characterData)){
+                if(characterData.hasOwnProperty("characterId")){
+                    characterIds.push(characterData);
+                }
+            }
+        }
+        else {
+            console.error(`Request failed with status ${response.status}`);
+        }
+
+
     }
     catch (error) {
         console.error('An error occurred:', error);
     }
         
+    //get all historical stats
+    //this can be done with just serverside 
+    try {
+        const response = await fetch('/d2wrapped/getHistoricalData', {
+            method: 'GET',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                destinyMembershipId: membershipId,
+                membershipType: membershipType,
+            })
+        });
+    
+        if (response.ok) {
+            const data = await response.json();
+            //load historical 'data' into database
+        }
+        else {
+            console.error(`Request failed with status ${response.status}`);
+        }
+
+
+    }
+    catch (error) {
+        console.error('An error occurred:', error);
+    }
+       
 });
+
+
 
 
     
